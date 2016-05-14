a59050ba94fedf67f5a768b97d797fd0f6e1b328
vmcbaptista <2054313@student.uma.pt>
2016-05-14 18:53:31 +0100

    Main test

--- Projeto IHC/src/projeto/ihc/ProjetoIHC.java
+++ Projeto IHC/src/projeto/ihc/ProjetoIHC.java
@@ -9,8 +9,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
 import javafx.stage.Stage;
-
 /**
  *
  * @author fabio
