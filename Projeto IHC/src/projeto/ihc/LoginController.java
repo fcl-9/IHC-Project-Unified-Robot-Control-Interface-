79583f76715bb146d8528880e47ced372a58d86f
Fabio <fabiocl93@gmail.com>
2016-05-16 19:14:44 +0100

    Ecrã de Login

---
+++ Projeto IHC/src/projeto/ihc/LoginController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto.ihc;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+
+
+/**
+ *
+ * @author fabio
+ */
+public class LoginController implements Initializable {
+    
+    private Label label;
+    @FXML
+    private AnchorPane background;
+    
+    private void handleButtonAction(ActionEvent event) {
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+    
+}
