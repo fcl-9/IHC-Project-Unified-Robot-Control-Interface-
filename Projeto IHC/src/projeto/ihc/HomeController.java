b8538283d0669bdfe87fb9824bc376d4691cddb0
vmcbaptista <2054313@student.uma.pt>
2016-05-17 21:01:23 +0100

    base semi terminada

--- Projeto IHC/src/projeto/ihc/HomeController.java
+++ Projeto IHC/src/projeto/ihc/HomeController.java
@@ -19,6 +19,11 @@ import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 
 /**
@@ -30,6 +35,10 @@ public class HomeController implements Initializable {
     private Label label;
     @FXML
     private AnchorPane background;
+    @FXML
+    private Pane map;
+    
+    MyBrowser myBrowser;
     
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
@@ -38,6 +47,29 @@ public class HomeController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        myBrowser = new MyBrowser();
+        map.getChildren().add(myBrowser);
+    }
+    }
+
+    class MyBrowser extends Region{
+
+        HBox toolbar;
+
+        WebView webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+
+        public WebEngine getWebEngine() {
+        return webEngine;
+    }
+
+    public MyBrowser(){
+
+        final URL urlGoogleMaps = getClass().getResource("/resources/maps.html");
+        webEngine.load(urlGoogleMaps.toExternalForm());
+
+        getChildren().add(webView);
+
     }    
     
 }
