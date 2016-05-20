e559737e7781b54e4a83e71cfa1eb0966aa8fec8
vmcbaptista <2054313@student.uma.pt>
2016-05-20 13:09:22 +0100

    inicio programacao login

--- Projeto IHC/src/projeto/ihc/LoginController.java
+++ Projeto IHC/src/projeto/ihc/LoginController.java
@@ -5,20 +5,26 @@
  */
 package projeto.ihc;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
+import javafx.stage.Stage;
 
 
 /**
@@ -30,6 +36,8 @@ public class LoginController implements Initializable {
     private Label label;
     @FXML
     private AnchorPane background;
+    @FXML
+    private Button loginButton;
     
     private void handleButtonAction(ActionEvent event) {
     }
@@ -37,5 +45,14 @@ public class LoginController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
     }    
+
+    @FXML
+    private void onLoginClicked(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Selecao.fxml"));
+        
+        Scene scene = new Scene(root);
+        Stage actualStage = (Stage) loginButton.getScene().getWindow();
+        actualStage.setScene(scene);
+    }
     
 }
