e5a8bb67132cf2886bc523f71ed1b13c20010487
AnaIsabel <AnaIsabel@Isabel>
2016-05-27 12:33:22 +0100

    Com a parte da ajuda

--- Projeto IHC/src/projeto/ihc/AjudaController.java
+++ Projeto IHC/src/projeto/ihc/AjudaController.java
@@ -5,9 +5,17 @@
  */
 package projeto.ihc;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +24,15 @@ import javafx.fxml.Initializable;
  */
 public class AjudaController implements Initializable {
 
+    @FXML
+    private AnchorPane background;
+    @FXML
+    private Button sair;
+    @FXML
+    private Button irDefinicoes;
+    @FXML
+    private Button retorna;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +40,39 @@ public class AjudaController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
+    @FXML
+    /**
+     * Este método fecha a interface
+     */
+    private void sairInterface(MouseEvent event) 
+    {
+        //stage é a janela, no botao vamos buscar a cena e a janela, para depois puder fechar
+        Stage stage = (Stage) sair.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    /**
+     * Este método muda para o ecrã das definições
+     */
+    private void IrParaDefinicoes(MouseEvent event) throws IOException 
+    {
+        //carrega tudo o que existe no ficheiro xml para puder ser utilizador na transição
+        Parent root = FXMLLoader.load(getClass().getResource("Definicoes.fxml"));
+        //mudança de ecrã, vai á cena e muda de ecra, vai para o ecra definicoes
+         irDefinicoes.getScene().setRoot(root);
+    }
+
+    @FXML
+     /**
+     * Este método muda para o ecrã home
+     */
+    private void voltarAtras(MouseEvent event) throws IOException 
+            
+    {    //carrega tudo o que existe no ficheiro xml para puder ser utilizador na transição
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        //muda de ecrã, é á cena e muda de ecrã, retorna oara o home
+        retorna.getScene().setRoot(root);
+    }
 }
