e199a0aeead9ad18ae245964ad4ce039618843d6
Fabio <fabiocl93@gmail.com>
2016-06-03 14:35:30 +0100

    Resolução adição de vars para controlo das cams

--- Projeto IHC/src/projeto/ihc/SensoresController.java
+++ Projeto IHC/src/projeto/ihc/SensoresController.java
@@ -104,6 +104,10 @@ public class SensoresController implements Initializable {
         }
     }
     
+    /**
+     * This method will indicate the robot that is select because of the transitions to previous screens
+     * @param name 
+     */
     public void setRobot (String name) {
         robot = name;
     }
